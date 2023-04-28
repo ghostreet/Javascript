b1fa6fcbdb2616d315fd5e1d83c2d1ec694ee033
ghostreet <vegacarripan@gmail.com>
2023-04-28 13:40:01 -0400

    arreglos y optimización del codigo

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-console.log("Bienvenido/a a la calculadora de notas 2023")
+console.log("Bienvenido/a a la calculadora de notas 2023");
 
 let consulta = confirm("¿Ingresar nuevo estudiante?")
-
-
-let nombre
-let nota1
-let nota2
-let nota3
+    
 let promedio
 
 let totalPromedio=0
 let cantidadEstudiantes=0
 
-
-while(consulta){
-nombre = prompt("Nombre del estudiante:");
-nota1 = parseInt(prompt("Ingresa la primera nota:"));
-nota2 = parseInt(prompt("Ingresa la segunda nota:"));
-nota3 = parseInt(prompt("Ingresa la tercera nota:"));
-
-function calcularProm(){
+function calcularProm(nombre, nota1, nota2, nota3){
     let promedio = (nota1+nota2+nota3)/3;
     totalPromedio+= promedio;
     cantidadEstudiantes+=1;
     return console.log(`El promedio de ${nombre} es: ${promedio.toFixed(2)}`);
 } 
-calcularProm();
 
-if (consulta = confirm("¿Desea agregar un nuevo estudiante?"));
+while(consulta){
+let nombre = prompt("Nombre del estudiante:");
+let nota1 = parseInt(prompt("Ingresa la primera nota:"));
+let nota2 = parseInt(prompt("Ingresa la segunda nota:"));
+let nota3 = parseInt(prompt("Ingresa la tercera nota:"));
+
+
+calcularProm(nombre, nota1, nota2, nota3);
+
+consulta = confirm("¿Desea agregar un nuevo estudiante?");
 
 
 }
